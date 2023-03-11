4cf61b11f63e2a33ea3ee93b8ae968f660eabd98
santanafullstack <jvcontrolle@gmail.com>
2023-03-11 17:05:50 -0300

    First Commit

---
+++ src/main/java/br/com/cotiinformatica/controller/AcessarContaController.java
@@ -0,0 +1,23 @@
+package br.com.cotiinformatica.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AcessarContaController {
+	
+	
+	
+	@RequestMapping(value= "/")
+	public ModelAndView acessarConta() {
+		
+		
+		ModelAndView modelAndView = new ModelAndView("/acessar-conta");	
+		
+		
+		return modelAndView;
+	       }
+	}
+
+
